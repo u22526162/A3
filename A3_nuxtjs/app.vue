3ae614d189782924296621350391bdc35eb044c5
Amadeus Fidos <u22526162@tuks.co.za>
2025-05-13 22:48:08 +0200

    files and nav component basics

--- A3_nuxtjs/app.vue
+++ A3_nuxtjs/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NavBar/>
   </div>
 </template>
---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar-container">
+      <NuxtLink to="/" class="navbar-logo">My Blog</NuxtLink>
+      <div class="navbar-links">
+        <NuxtLink to="/" class="navbar-link">Home</NuxtLink>
+        <NuxtLink to="/search" class="navbar-link">Search</NuxtLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  // No inline styles here
+}
+</script>
+
+<style scoped src="@/assets/css/navbar.css"></style>
